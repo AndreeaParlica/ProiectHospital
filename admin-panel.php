3b67551754818c2dcaadc873af2a2f49ac983373
AndreeaParlica <andreea.vasile88@gmail.com>
2022-12-03 18:27:05 +0200

    heroku db setup

--- admin-panel.php
+++ admin-panel.php
@@ -2,6 +2,16 @@
 <?php 
 include('func.php');  
 include('newfunc.php');
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+
 $con=mysqli_connect("localhost","root","","myhmsdb3");
 
 
