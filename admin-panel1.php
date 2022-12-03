4a3c9860f009b6616b86674aa564eb8f438634cc
AndreeaParlica <andreea.vasile88@gmail.com>
2022-12-03 21:12:37 +0200

    connection with heroku db in admin-panel1

--- admin-panel1.php
+++ admin-panel1.php
@@ -344,7 +344,16 @@ if(isset($_POST['docsub1']))
                 </thead>
                 <tbody>
                   <?php 
-                    $con=mysqli_connect("localhost","root","","myhmsdb3");
+                    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+                    $cleardb_server = $cleardb_url["host"];
+                    $cleardb_username = $cleardb_url["user"];
+                    $cleardb_password = $cleardb_url["pass"];
+                    $cleardb_db = substr($cleardb_url["path"],1);
+                    $active_group = 'default';
+                    $query_builder = TRUE;
+                    
+                    $con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+                    // $con=mysqli_connect("localhost","root","","myhmsdb3");
                     global $con;
                     $query = "select * from patreg";
                     $result = mysqli_query($con,$query);
