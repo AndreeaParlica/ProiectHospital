233aa3510bc77596e0d15d88a2301c54c5ef38cb
AndreeaParlica <andreea.vasile88@gmail.com>
2023-01-21 20:48:24 +0200

    validation captcha

--- contact.php
+++ contact.php
@@ -26,35 +26,35 @@ if(isset($_POST['btnSubmit']))
 	if($result)
     {
     	echo '<script type="text/javascript">'; 
-		echo 'alert("Message sent successfully!");'; 
+		echo 'alert("Messaj trimis cu succes!");'; 
 		echo 'window.location.href = "contact.html";';
 		echo '</script>';
     }
 }
-// session_start();
-// //verifica captcha code
-// 	if(!empty($_POST["btnSubmit"])) {
-// 	$name = $_POST["txtName"];
-// 	$email = $_POST["txtEmail"];
-// 	$captcha = $_POST["captcha"];
-// 	$captchaUser = filter_var($_POST["captcha"]);
-// 	if(empty($captcha)) {
-// 	  $captchaError = array(
-// 		"status" => "alert-danger",
-// 		"message" => "Inserati un cod captcha."
-// 	  );
-// 	}
-// 	else if($_SESSION['CAPTCHA_CODE'] == $captchaUser){
-// 	  $captchaError = array(
-// 		"status" => "alert-success",
-// 		"message" => "Formular trimis cu succes."
-// 	  );
-// 	} else {
-// 	  $captchaError = array(
-// 		"status" => "alert-danger",
-// 		"message" => "Captcha invalid."
-// 	  );
-// 	}
-//   }  
+
+//verifica captcha code
+	if(!empty($_POST["btnSubmit"])) {
+	$name = $_POST["txtName"];
+	$email = $_POST["txtEmail"];
+	$captcha = $_POST["captcha"];
+	$captchaUser = filter_var($_POST["captcha"]);
+	if(empty($captcha)) {
+	  $captchaError = array(
+		"status" => "alert-danger",
+		"message" => "Inserati un cod captcha."
+	  );
+	}
+	else if($_SESSION['CAPTCHA_CODE'] == $captchaUser){
+	  $captchaError = array(
+		"status" => "alert-success",
+		"message" => "Formular trimis cu succes."
+	  );
+	} else {
+	  $captchaError = array(
+		"status" => "alert-danger",
+		"message" => "Captcha invalid."
+	  );
+	}
+  }  
 
 ?>
